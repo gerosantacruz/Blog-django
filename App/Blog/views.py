c985b9d675d3124a2bce9ad587fdbddb8f11a9ad
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-02-16 19:51:43 -0300

    The index view for post

--- App/Blog/views.py
+++ App/Blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm
+from .models import Post
+from django.views.generic import ListView, DetailView 
 
 # Create your views here.
+#Home: this views display the list of post
+class IndexView(ListView):
+    template_name='Blog/index.html'
+    context_object_name = 'post_list'
+    def get_queryset(self):
+        return Post.objects.all()
+    
