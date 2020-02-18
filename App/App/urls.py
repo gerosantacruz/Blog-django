b008ee176c9749d1a47e89e607c410ee3ff767cf
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-02-18 09:29:22 -0300

    models and view completed, started with the template

--- App/App/urls.py
+++ App/App/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>', views.PostDetailView.as_view(), name='detail'),
+    path('edit/<int:pk>/', views.edit, name='edit'),
+    path('post/', views.postview, name='post'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
 ]
--- App/Blog/views.py
+++ App/Blog/views.py
@@ -8,6 +8,39 @@ from django.views.generic import ListView, DetailView
 class IndexView(ListView):
     template_name='Blog/index.html'
     context_object_name = 'post_list'
+
     def get_queryset(self):
         return Post.objects.all()
+# view post detail
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'Blog/post-detail.html'
+
+#Create a new post
+def postview(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+    form = PostForm()
+    return render(request, 'Blog/post.html', {'form':form})
+
+#Edit the post element
+def edit(request, pk, template_name='Blog/edit.html'):
+    post= get_object_or_404(Post, pk=pk)
+    form = PostForm(request.POST or none, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, template_name, {'form':form})
+
+#delete the post element
+def delete(request, pk, template_name='Blog/confirm_delete.html'):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('index')
+    return render(request, template_name, {'object': post})
+        
     
